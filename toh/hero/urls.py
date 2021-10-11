86b3fc5a5332624bc5999b86bcd10eefbdfdda81
ttoru96 <ttoru96jk@gmail.com>
2021-10-11 22:18:36 +0900

    Finish with all practices

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('<int:id>/', views.id, name='get_id'),
-    path('<str:name>/', views.name, name='get_name')
+    # path('', views.index, name='index'), 
+    # path('<int:id>/', views.id, name='get_id'),
+    # path('<str:name>/', views.name, name='get_name')
+    path('', views.hero_list)
 ]
--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.http.response import HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+from json.decoder import JSONDecodeError
+
+from .models import Hero
 
 def index(request):
     return HttpResponse('Hello, world!')
@@ -8,3 +15,23 @@ def id(request, id):
 
 def name(request, name):
     return HttpResponse(f'Your name is {name}!')
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == "GET":
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == "POST":
+        # TODO: create a new hero
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)['name']
+            hero_age = json.loads(body)['age']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name, age=hero_age)
+        hero.save()
+        response_dict = {'id': hero.id, 'name': hero.name, 'age': hero.age}
+        return JsonResponse(response_dict, status=201)
+    else: 
+        return HttpResponseNotAllowed(["GET", "POST"])
