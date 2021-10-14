0fe3c9b8038fb25400738c24ba37cf2c206347c3
zz-zye <htht2808@gmail.com>
2021-10-14 21:27:12 +0900

    practice part 3

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -5,9 +5,13 @@ from django.db import models
 class Hero(models.Model) :
   name = models.CharField(max_length=120)
   age = models.IntegerField(default=0)
+  score = models.IntegerField(default=0)
 
   def __str__(self) :
     return self.name
+  
+  def introduce(self) :
+    return 'Hello, my name is {} and my score is {}!'.format(self.name, self.score)
 
 class Team(models.Model) :
   name = models.CharField(max_length=120)
