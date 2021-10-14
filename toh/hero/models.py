1d0ed64310fe1b24493bfbaf3f54b60163329802
ccdc-git <titieiti.com@gmail.com>
2021-10-14 20:52:52 +0900

    task 3

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=120)
     age = models.IntegerField(blank=True, null=True)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+    def introduce(self):
+        print('Hello, my name is {} and my score is {}!'.format(self.name, self.score))
 
 
 class Team(models.Model):
@@ -23,4 +27,3 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
-
