b00909fc4a14ac2026b1286beb4ecda931f9b99b
YCyoon1994 <withtogetherapp@gmail.com>
2021-10-14 21:54:01 +0900

    pratice #7 tasks 1,2,3 - 2

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Hero(models.Model):
     name = models.CharField(max_length=120)
-    age =  models.IntegerField(blank=True, null=True)
+    age =  models.IntegerField(default=0, blank=True, null=True)
     score =  models.IntegerField(default=0, blank=True, null=True)
 
     def __str__(self):
