2d6af8e9702f296d0dc737ba34dccdc680e270a6
Myeongseop Jeon <jeonms1611@gmail.com>
2021-10-14 21:04:04 +0900

    Do Task 3

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -4,9 +4,24 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=120)
     age = models.IntegerField(default=25)
+    score = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return str(self.name)
 
     def to_dict(self) -> dict:
         return {"id": self.id, "name": self.name, "age": self.age}
+    
+    def introduce(self) -> str:
+        return f"Hello, my name is {self.name} and my score is {self.score}!"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=120)
+    leader = models.ForeignKey(
+        Hero, on_delete=models.CASCADE, related_name="leader_set"
+    )
+    members = models.ManyToManyField(Hero, related_name="teams")
+
+    def __str__(self) -> str:
+        return str(self.name)
