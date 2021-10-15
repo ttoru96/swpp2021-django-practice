ddf2f4e9ca33bbe973f03a18688490040e1b2229
silvermanseoul <silverman.unicorn@gmail.com>
2021-10-15 20:48:27 +0900

    feat: Add score and introduce() to Hero

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=120)
     age = models.IntegerField(blank=True, null=True)
+    score = models.IntegerField(default=0)
+
+    def introduce(self):
+        return f'Hello, my name is {self.name} and my score is {self.score}!'
 
     def __str__(self):
         return self.name
