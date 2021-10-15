e84400fae6c72855a5cac3ed05804e8eb3744df3
ddony8128 <ddoddony@naver.com>
2021-10-15 15:56:14 +0900

    Today's task 3

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=120)
     age = models.IntegerField(blank=True, null=True)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+    
+    def introduce(self):
+        return f"Hello, my name is {self.name} and my score is {self.score}!"
 
 class Team(models.Model):
     name = models.CharField(max_length=120)
