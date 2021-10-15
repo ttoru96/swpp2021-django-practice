3f1cc9e2f473b0acc5c5c44b98913c048e62a5c7
KIMJunOh <kjo1797@gmail.com>
2021-10-15 19:08:25 +0900

    task

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.hero_list),
+    #path('', views.index, name='index'),
     path('<int:id>/', views.hero_id),
     path('<str:name>/', views.hero_name),
 ]
