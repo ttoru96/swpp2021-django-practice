1bc5f43b3f6b189ee17773d79e45fa6d6a412c60
silvermanseoul <silverman.unicorn@gmail.com>
2021-10-15 20:29:45 +0900

    feat: Implement hero app

---
+++ toh/hero/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Hero(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=120)
+    leader = models.ForeignKey(
+        Hero,
+        on_delete=models.CASCADE,
+        related_name='leader_set'
+    )
+    members = models.ManyToManyField(
+        Hero,
+        related_name='teams'
+    )
+
+    def __str__(self):
+        return self.name
---
+++ toh/hero/views.py
@@ -0,0 +1,26 @@
+import json
+from json import JSONDecodeError
+
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Hero
+
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == 'GET':
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == 'POST':
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)['name']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {'id': hero.id, 'name': hero.name}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
