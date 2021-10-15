6ee7f390cd7bfbada86e18f8da50a9ba91d70656
tippy <tippy@naver.com>
2021-10-15 16:37:31 +0900

    first task done

---
+++ toh/hero/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.hero_list),
+    path('<int:id>/', views.hero_id),
+    path('<str:name>/', views.hero_name)
+]
---
+++ toh/hero/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+from json.decoder import JSONDecodeError
+from .models import Hero
+
+def hero_id(request, id=1):
+    return HttpResponse('Your name is %d!' % id)
+
+def hero_name(request, name=""):
+    return HttpResponse('Your name is %s!' % name)
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == "GET":
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == "POST":
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)["name"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {"id": hero.id, "name": hero.name}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(["GET", "POST"])
