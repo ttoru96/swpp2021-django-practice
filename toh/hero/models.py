1a230736a704f2507ecbc478900cec91097a618d
Zagoshipda <john713@hanmail.net>
2021-10-16 00:04:34 +0900

    Update : task 2

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Hero(models.Model):
+  name = models.CharField(max_length=120)
+  age = models.IntegerField(blank=True, null=True)
+
+  def __str__(self):
+    return self.name
--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  # path('', views.index, name='index'),
+  path('', views.hero_list),
+  path('info/<int:id>/', views.hero_info),
   path('<int:id>/', views.hero_id, name='hero_id'),
   path('<str:name>/', views.hero_name, name='hero_name'),
 ]
