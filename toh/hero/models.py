e36c4e2bb4434513313b366fac0f47bb5de609ed
ddony8128 <ddoddony@naver.com>
2021-10-15 15:47:21 +0900

    Today's task 2

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length=120)
+    age = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=120)
+    leader = models.ForeignKey(
+        Hero,
+        on_delete=models.CASCADE,
+        related_name='leader_set',
+    )
+    members = models.ManyToManyField(
+        Hero,
+        related_name='teams',
+    )
+
+    def __str__(self):
+        return self.name
--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+from json.decoder import JSONDecodeError
+
+from .models import Hero
 
 def index(request):
     return HttpResponse('Hello, world!')
@@ -8,3 +15,43 @@ def hero_id(request, id=0):
 
 def hero_name(request, name=""):
     return HttpResponse(f'Your name is {name}!')
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == "GET":
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == "POST":
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)["name"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {"id": hero.id, "name": hero.name}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(["GET","POST"])
+
+@csrf_exempt
+def hero_info(request, id):
+    if request.method == "GET":
+        hero = Hero.objects.get(id=id)
+        response_dict = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(response_dict)
+    elif request.method == "PUT":
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)["name"]
+            hero_age = json.loads(body)["age"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero.objects.get(id=id)
+        hero.name = hero_name
+        hero.age = hero_age
+        hero.save()
+        response_dict = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(["GET","PUT"])
